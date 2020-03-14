4ca337534bdad1d03d77bb30ee31a6a1f659abea
Zbigniew Downar <zbyszrom@Zbigniews-iMac.local>
2020-03-14 15:46:51 +0100

    gallery

--- src/components/forGallery.js
+++ src/components/forGallery.js
@@ -1,8 +1,40 @@
 import React from 'react'
+import {useStaticQuery, graphql} from 'gatsby'
+import Img from 'gatsby-image'
+
+const getGallery = graphql `
+query {
+galleryImg: allFile(sort: {fields: name}, filter: {relativeDirectory: {eq: "gallery"}}) {
+      edges {
+        node {
+          childImageSharp {
+            fluid {
+                ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 const ForGallery = ()=>{
+
+    const data = useStaticQuery(getGallery)
+    const galImages = data.galleryImg.edges
+
     return(
-       <h3>Gallery</h3> 
+        <div>
+            <h3>Gallery</h3> 
+          {galImages.map (
+              ({node}, i) => (
+                <Img key={i} fluid = {node.childImageSharp.fluid}/>
+              )
+          )}
+            
+        </div>
+       
+
     )
 }
 
